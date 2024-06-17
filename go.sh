73c930884d164acf19dd5d4498217d07f47ff631
Faisal Saeed <faisal.saeed@scylladb.com>
2024-06-18 00:34:44 +0800

    Environment loader script

---
+++ go.sh
@@ -0,0 +1,19 @@
+ENV=".env"
+
+if [ ! -f "$ENV" ]; then
+    echo "'.env' file not found!"
+    echo "Please review the env-sample file for instructions"
+    echo
+    exit 1
+fi
+source $ENV
+
+if [ -z "$TF_VAR_scylla_api_token" ]; then
+    echo "The variable TF_VAR_scylla_api_token has an invalid Scylla Cloud token!"
+    echo 
+    exit 1
+fi
+
+echo "Proceed to execute 'terraform' script..."
+echo
+exit 0
