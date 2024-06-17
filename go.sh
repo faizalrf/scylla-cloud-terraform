64a855bbabff34069d310a888314c0c8889a2e8d
Faisal Saeed <faisal.saeed@scylladb.com>
2024-06-18 00:40:53 +0800

    Fixed typo

--- go.sh
+++ go.sh
@@ -1,4 +1,6 @@
-ENV=".env"
+#!/bin/bash
+
+ENV="./.env"
 
 if [ ! -f "$ENV" ]; then
     echo "'.env' file not found!"
@@ -6,14 +8,15 @@ if [ ! -f "$ENV" ]; then
     echo
     exit 1
 fi
-source $ENV
+
+source "$ENV"
 
 if [ -z "$TF_VAR_scylla_api_token" ]; then
     echo "The variable TF_VAR_scylla_api_token has an invalid Scylla Cloud token!"
     echo 
     exit 1
+else
+    echo "Proceed to execute 'terraform' script..."
+    echo
+    exit 0
 fi
-
-echo "Proceed to execute 'terraform' script..."
-echo
-exit 0
